6ec886399211791ac99518a95caef9b20d9d1632
amybohbeanii <jiaxuan@brandeis.edu>
2017-01-28 23:27:20 -0500

    Create background.js

---
+++ background.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	function addTriangleTo(target,inputColor) {
+	    var dimensions = target.getClientRects()[0];
+	    var pattern = Trianglify({
+	        width: screen.width,
+	        height: dimensions.height,
+			x_colors: inputColor,
+			y_colors: 'match_x'
+	    });
+	    target.style['background-image'] = 'url(' + pattern.png() + ')';
+	}
+
+	addTriangleTo(document.getElementById('home'),'PRGn');
+	addTriangleTo(document.getElementById('education'),'Spectral');	
+	addTriangleTo(document.getElementById('work-experience'),'GnBu');	
+	addTriangleTo(document.getElementById('projects'),'PuOr');	
+})
+
+
+
